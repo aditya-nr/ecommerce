3dd74ebaadd9f54493ecf3a9cfe7c58e2cf8d98d
aditya <adityanarayanrajan@gmail.com>
2024-07-11 06:11:04 +0530

    logo edit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Logo = ({ className }) => {
     return (
         <Link to={'/'} className={`text-lg font-bold italic text-slate-400 ${className}`}>
-            Just<span className='text-blue-400'>Buy</span>
+            Just<span className='text-blue-400'>Buyy</span>
         </Link >
     )
 }
